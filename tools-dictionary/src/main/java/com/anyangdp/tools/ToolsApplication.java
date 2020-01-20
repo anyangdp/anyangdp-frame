6c34af0074185280b3925c24ac1d5d676998e865
anyangdp@aliyun <anyangdp@aliyun>
2020-01-20 15:04:11 +0800

    数据库配置；日志配置；

--- tools-dictionary/src/main/java/com/anyangdp/tools/ToolsApplication.java
+++ tools-dictionary/src/main/java/com/anyangdp/tools/ToolsApplication.java
@@ -1,9 +1,18 @@
 package com.anyangdp.tools;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @Author anyang
  * @CreateTime 2020/1/20
  * @Des
  */
+@EnableEurekaClient
+@SpringBootApplication
 public class ToolsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsApplication.class, args);
+    }
 }
